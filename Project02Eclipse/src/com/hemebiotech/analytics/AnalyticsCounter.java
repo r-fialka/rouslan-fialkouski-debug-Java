e4b9044c3a5ac3c283deefd6c221a8ce30835dc3
r-fialka <ruslan.fialkovsk@yahoo.com>
2025-08-09 09:28:35 +0200

    remove unnecessary comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,38 +1,43 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.FileWriter;
 
-public class AnalyticsCounter {	
+public class AnalyticsCounter {
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
 	
 	public static void main(String args[]) throws Exception {
-		countSymptoms();	
-	}
-		
-	public static void countSymptoms() {
-			
-		// Créons une liste pour les mots et leur nombre.
-		Map<String, Integer> listeSymptoms = new HashMap<>();
-			
-		WriteSymptomDataToFile write = new WriteSymptomDataToFile();
-						
-		try (BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"))) {
-			String symptom;
-			while ((symptom = reader.readLine()) != null) {
-				listeSymptoms.put(symptom, listeSymptoms.getOrDefault(symptom, 0) + 1); //Comptage des symptômes
+
+		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		String line = reader.readLine();
+
+		int i = 0;	
+		int headCount = 0;
+		while (line != null) {
+			i++;
+			System.out.println("symptom from file: " + line);
+			if (line.equals("headache")) {
+				headCount++;
+				System.out.println("number of headaches: " + headCount);
 			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Le fichier symptoms.txt n'a pas été trouvé..");
-			//return;  //Sortir de la méthode si le fichier n'existe pas
-		} catch (IOException e) {
-			System.out.println("Erreur de lecture du fichier symptoms.txt");
-			return;
+			else if (line.equals("rush")) {
+				rashCount++;
+			}
+			else if (line.contains("pupils")) {
+				pupilCount++;
+			}
+
+			line = reader.readLine();	
 		}
-			
-			write.writeSymptoms(listeSymptoms);	
+		
+
+		FileWriter writer = new FileWriter ("result.out");
+		writer.write("headache: " + headacheCount + "\n");
+		writer.write("rash: " + rashCount + "\n");
+		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.close();
 	}
 }
